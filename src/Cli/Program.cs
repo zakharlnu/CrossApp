27a1be1bd6406167939d7d43673da7fa4cb4ab16
Zakhar Ilchuk <zakhar.ilchuk@lnu.edu.ua>
2026-09-07 18:01:26 +0300

    lab01: solution CrossApp, Cli, вибір домену

---
+++ src/Cli/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
+Console.WriteLine("Студент: Ільчук Захар, група ФЕІ-32");
+Console.WriteLine(new string('-', 52));
+
+Console.WriteLine($"ОС (OSDescription) : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
+Console.WriteLine($"Runtime : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог : {Environment.CurrentDirectory}");
+
+Console.WriteLine(new string('-', 52));
+Console.WriteLine( "Предметна область: Замовлення (клієнти, товари, замовлення)");
